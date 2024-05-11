7d72217d46edc564fd608fd752604886e01e6d56
evadow <eva@villagewebcompany.co.uk>
2024-05-11 12:25:55 +0100

    howdypartner.php

--- helloWorthy.php
+++ helloWorthy.php
@@ -1,33 +1,28 @@
 <?php
 /**
- * @package Hello_Worthy
+ * @package Howdy_Partner
  * @version 1.2
  */
 /*
-Plugin Name: Hello Worthy
-Plugin URI: http://wordpress.org/extend/plugins/hello-worthy/
-Description: This is based on the Hello Dolly plugin that comes as a standard install of all WordPress installations. Instead of lyrics to the song Hello, Dolly, this plugin will display a quote, reminder or other comment.
-Author: Claire Worthington
+Plugin Name: Howdy Partner
+Plugin URI: http://wordpress.org/extend/plugins/howdy-partner/
+Description: This is based on the Hello Dolly plugin that comes as a standard install of all WordPress installations. Instead of lyrics to the song Hello, Dolly, this plugin will display a a lyric from Jamiroquai's Space Cowboy aka the greatest song of all time.
+Author: Eva Dowling
 Version: 1.0
-Author URI: http://worthyontheweb.co.uk
+Author URI: N/A
 */
 
-function hello_worthy_get_quote() {
+function howdy_partner_get_quote() {
 	/** These are the lyrics to I love your smile and some random quotes*/
-	$lyrics = "Hello, Worthy,
-	Be ready to evolve,
-	Always work as a team,
-	Improvement is necessary,
-	Ask the dumb question,
-	Don't underestimate the power of design skills,
-	Work closely with your customers,
-	Go beyond just writing code,
-	Pay close attention to the architecture,
-	Consider accessibility,
-	A cup of tea makes everything better,
-	Smile and the whole world smiles with you,
-	Why fit in when you were born to stand out - Dr Seuss,";
-
+	$lyrics = "Everything is good and brown,
+	With a sunshine smile upon my face,
+	And all my inhibitions have disappeared without a trace,
+	This is the return of the space cowboy,
+	I can see clearly! so high in sky,
+	Oh, good times, hard times, good vibes, hey!
+	At the speed of cheeba,
+        Gotta go, gotta go, got to go,";
+	
 	// Here we split it into lines
 	$lyrics = explode( "\n", $lyrics );
 
@@ -36,22 +31,22 @@ function hello_worthy_get_quote() {
 }
 
 // This just echoes the chosen line, we'll position it later
-function hello_worthy() {
-	$chosen = hello_worthy_get_quote();
-	echo "<p id='worthy'>$chosen</p>";
+function howdy_partner() {
+	$chosen = howdy_partner_get_quote();
+	echo "<p id='cowboy'>$chosen</p>";
 }
 
 // Now we set that function up to execute when the admin_notices action is called
-add_action( 'admin_notices', 'hello_worthy' );
+add_action( 'admin_notices', 'howdy_partner' );
 
 // We need some CSS to position the paragraph
-function worthy_css() {
+function partner_css() {
 	// This makes sure that the positioning is also good for right-to-left languages
 	$x = is_rtl() ? 'left' : 'right';
 
 	echo "
 	<style type='text/css'>
-	#worthy {
+	#partner {
 		float: $x;
 		padding-$x: 15px;
 		padding-top: 5px;		
@@ -62,6 +57,6 @@ function worthy_css() {
 	";
 }
 
-add_action( 'admin_head', 'worthy_css' );
+add_action( 'admin_head', 'partner_css' );
 
 ?>
